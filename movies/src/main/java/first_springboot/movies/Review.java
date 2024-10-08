0f1cae0555d66fc6bf8ec254aa53e78730a1ca19
gordonwun <gordonwun99@gmail.com>
2024-10-09 02:10:07 +0800

    GetAll

---
+++ movies/src/main/java/first_springboot/movies/Review.java
@@ -0,0 +1,19 @@
+package first_springboot.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review {
+    @Id
+    private ObjectId Id;
+    private String body;
+}
