e6618749d21cee4f9c0aa008885c28122e8f9a25
gordonwun <gordonwun99@gmail.com>
2024-10-09 20:34:12 +0800

    setCors

--- movies/src/main/java/first_springboot/movies/ReviewController.java
+++ movies/src/main/java/first_springboot/movies/ReviewController.java
@@ -6,12 +6,14 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/api/v1/reviews")
 public class ReviewController {
     @Autowired
