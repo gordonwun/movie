748479dfd19e4b2ca23d6b9faf38322e324ba811
gordonwun <gordonwun99@gmail.com>
2024-10-09 20:36:44 +0800

    React first commit

---
+++ MovieClient/movie-react/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import api from './api/axiosConfig'
+
+
+function App() {
+
+  const [movies, setMovies] = useState()
+
+  const getMovies = async () => {
+    try {
+
+      const response = await api.get("/api/v1/movies")
+
+      console.log(response.data)
+      
+      setMovies(response.data)
+    } 
+    catch (error) {
+      console.log(error)
+    }
+      
+  }
+
+  useEffect(() => {
+    getMovies()
+  },[])
+
+  return (
+    <>
+    <div className='App'>
+
+
+    </div>
+    </>
+  )
+}
+
+export default App
